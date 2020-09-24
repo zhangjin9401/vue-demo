09dd22ef5d03ac95722b771a511c28dfdde33f51
张金 <zhangjin8@xiaomi.com>
2020-09-24 10:10:06 +0800

    调试vue生命周期

--- src/App.vue
+++ src/App.vue
@@ -1,119 +1,55 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-29 11:16:30
- * @LastEditTime: 2020-07-31 16:53:13
+ * @LastEditTime: 2020-09-23 19:58:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-demo/src/App.vue
 --> 
 <template>
   <div id="app">
-    <!-- vue不能检测到对象属性的添加或删除 -->
-    <!-- <one-card :obj="obj" :obj2="obj2"></one-card> -->
-    <h1 v-if="obj1 && obj1.a">obj1.a{{obj1.a}}</h1>
-    <h1 v-if="obj1 && obj1.b">obj1.b{{obj1.b}}</h1>
-    <h1 v-if="obj1">obj1{{obj1}}</h1>
-    <button @click="setObj1a">给obj1添加属性a</button>
-    <button @click="setObj1b">给obj1添加属性b</button>
-    <button @click="setObj1ab">更改obj1添加属性ab</button>
-    <button @click="againSetObj1ab">再次更改obj1添加属性ab</button>
-    <button @click="againSetObj1abTwo">再次第二次更改obj1添加属性ab</button>
-    <button @click="againSetObj1abThree">再次第三次更改obj1添加属性ab</button>
-    <div v-for="(item, index) in arr1" :key="index">{{item}}</div>
-    <button @click="addArr1">点击添加arr1属性</button>
-    <button @click="delArr1">点击删除arr1属性</button>
-    <button @click="changeArr1">点击更改arr1</button>
-    <button @click="changeArr1Two">点击再次更改arr1</button>
+    <button @click="toggleOneCard">切换oneCrad</button>
+    <button @click="addOneCount">oneCount自增</button>
+    <button @click="shiftOneCount">oneCount自减</button>
+    <one-card v-if="showOneCard" :one-count="oneCount"></one-card>
   </div>
 </template>
 
 <script>
-
+import OneCard from './components/OneCard'
 export default {
   name: 'App',
   components: {
+    OneCard
   },
   data(){
     return{
-      obj1:{
-
-      },
-      arr1:[{
-        amy:{
-          like:{
-            sports:['run']
-          }
+      showOneCard: true,
+      oneCount:{
+        age:1,
+        colors:{
+          like:['r','e','d']
         }
-      }]
+      }
     }
   },
+  created(){
+    console.log('app created')
+  },
+  mounted(){
+    console.log('app mounted')
+  },
   methods:{
-    addArr1(){
-      this.arr1[0].amy.like.sports.push('ball')
-    },
-
-    delArr1(){
-      this.arr1[0].amy.like.sports.shift()
-    },
-
-    changeArr1(){
-      let newObj = {bob:{like:{sports:['ball']}}}
-      this.arr1.push(newObj)
-    },
-
-    changeArr1Two(){
-      // this.arr1[0].amy.cat = 'amity'
-      // this.arr1.splice(0,1,{
-      //   amy:{
-      //     cat:'amity',
-      //     like:{
-      //       sports:['run']
-      //     }
-      //   }
-      // })
-      this.$set(this.arr1[0].amy,'cat','amity')
-    },
-
-    setObj1a(){
-      // this.obj1.a = 'hello obj1 a'
-      this.obj1 = Object.assign(this.obj1,{a:'hello obj1 a'})
-      this.$set(this.obj1, 'a', 'hello obj1 a')
-    },
-    setObj1b(){
-      // this.obj1.b = 'hello obj1 b'
-      this.obj1 = Object.assign({},{b:'hello obj1 b'})
-    },
-    setObj1ab(){
-      this.obj1 = Object.assign({},{a:'a',b:'b'})
-    },
-    againSetObj1ab(){
-      this.obj1 = Object.assign({},{
-        a:{
-          a1:'a1',
-          a2:'a2'
-        },
-        b:{
-          b1:{
-            b10:['green']
-          }
-        }
-      })
-    },
-    againSetObj1abTwo(){
-      this.obj1 = Object.assign({},{
-        a:{
-          a1:'a1',
-        },
-        b:{
-          b1:{
-            b10:[]
-          }
-        }
-      })
-    },
-    againSetObj1abThree(){
-      
-    },
+   toggleOneCard(){
+     this.showOneCard = !this.showOneCard
+     this.oneCount.colors.push('6')
+   },
+   addOneCount(){
+     this.oneCount.colors.like.splice(3,0,'s')
+   },
+   shiftOneCount(){
+     this.oneCount.colors.like[0] = 'g'
+   }
   }
 }
 </script>
---
+++ src/test.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-11 10:27:29
+ * @LastEditTime: 2020-08-11 10:32:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue-demo/src/test.js
+ */
+console.log(012)
+// console.log(module)
+console.log(exports)
+console.log(import)
